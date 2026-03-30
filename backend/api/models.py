4c0ca67c304954e4bd7b44c02444165faa14e828
richard.nguyen1 <richard.nguyen2005@outlook.com>
2026-03-29 21:12:43 -0600

    Made more tables, will work on Tuesday - Richard

--- backend/api/models.py
+++ backend/api/models.py
@@ -17,3 +17,42 @@ class Business(models.Model):
   Business_Name = models.CharField(max_length = 256)
   Business_Balance = models.FloatField(default = 0.0)
   Business_Profile = models.CharField(max_length = 256)
+  
+class Site_Admin(models.Model):
+  User_ID = models.OneToOneField(User, on_delete = models.CASCADE, on_update = models.CASCADE)
+
+# class Business_Admin(models.Model):
+  
+class Transaction(models.Model):
+  # Note: Foreign Key is beest used for one to many relationships
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE, on_update = models.CASCADE)
+  User_ID = models.ForeignKey(User, on_delete = models.CASCADE, on_update = models.CASCADE)
+  # adds date automatically
+  Transaction_Date = models.DateField(auto_now_add=True)
+
+# Invoice and Expense Pay Off already connected to Transaction, which means we don thave
+class Inovice(models.Model):
+  Name = models.CharField(max_length= 256)
+  Transaction_ID = models.OneToOneField(Transaction, on_delete = models.CASCADE, on_update = models.CASCADE)
+  Has_Paid = models.BooleanField(default = False)
+  Policy_Description = models.CharField(max_length = 256)
+
+# class Expense_Plan(models.Model):
+  
+# class Expense_Pay_Off(models.Model):
+
+# class Expense(models.Model):
+
+# class Employee(models.Model):
+
+# class Moderates
+
+# class RecurringPlan
+
+# class Contract
+
+# class InvoiceLineItem
+
+# class Business_Client
+
+# class Individual_Client
