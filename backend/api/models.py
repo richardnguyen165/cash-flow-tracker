8ade7d80a58cf77ade6fd62073711d8e39a84c83
richard.nguyen1 <richard.nguyen2005@outlook.com>
2026-03-29 18:19:40 -0600

    made 2 tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  # Note: id is automatically added
+  User_FName = models.CharField(max_length = 256)
+  User_LName = models.CharField(max_length = 256)
+  User_IsSiteAdmin = models.BooleanField(default = False)
+  User_IsEmployed = models.BooleanField(default = False)
+  User_Balance = models.FloatField(default = 0.0)
+  User_Password = models.CharField(max_length= 256)
+  User_BirthDate = models.DateField()
+  User_Email = models.CharField(max_length = 256)
+  User_Profile = models.CharField(max_length = 256)
+
+class Business(models.Model):
+  Business_Name = models.CharField(max_length = 256)
+  Business_Balance = models.FloatField(default = 0.0)
+  Business_Profile = models.CharField(max_length = 256)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME", "fct_db"),
+        'USER': os.getenv("DB_USER", "root"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST_NAME", "localhost"),
+        'PORT': os.getenv("DB_PORT", "3306"),
     }
 }
 
