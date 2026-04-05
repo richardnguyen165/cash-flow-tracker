aac14ccb81d5cd690d04e59c2093f748235f3179
30191307 <mamadou.diallo@ucalgary.ca>
2026-04-05 05:58:05 -0600

    added 8 more tables. Still have Employee, Contract and Individual client to be done but they're  ambiguous to me.

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,40 +19,69 @@ class Business(models.Model):
   Business_Profile = models.CharField(max_length = 256)
   
 class Site_Admin(models.Model):
-  User_ID = models.OneToOneField(User, on_delete = models.CASCADE, on_update = models.CASCADE)
+  User_ID = models.OneToOneField(User, on_delete = models.CASCADE)
 
-# class Business_Admin(models.Model):
+
+class Business_Admin(models.Model):
+  User_ID = models.OneToOneField(User, on_delete = models.CASCADE)
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+
   
 class Transaction(models.Model):
   # Note: Foreign Key is beest used for one to many relationships
-  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE, on_update = models.CASCADE)
-  User_ID = models.ForeignKey(User, on_delete = models.CASCADE, on_update = models.CASCADE)
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
   # adds date automatically
   Transaction_Date = models.DateField(auto_now_add=True)
 
 # Invoice and Expense Pay Off already connected to Transaction, which means we don thave
 class Inovice(models.Model):
   Name = models.CharField(max_length= 256)
-  Transaction_ID = models.OneToOneField(Transaction, on_delete = models.CASCADE, on_update = models.CASCADE)
+  Transaction_ID = models.OneToOneField(Transaction, on_delete = models.CASCADE)
   Has_Paid = models.BooleanField(default = False)
   Policy_Description = models.CharField(max_length = 256)
 
-# class Expense_Plan(models.Model):
-  
+
+class Expense_Plan(models.Model):
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  Plan_Title = models.CharField(max_length = 256)
+  Expense_Plan_Due = models.DateField()
 # class Expense_Pay_Off(models.Model):
-
+class Expense_Pay_Off(models.Model):
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  Transaction_ID = models.OneToOneField(Transaction, on_delete = models.CASCADE)
+  Total_Pay = models.FloatField(default = 0.0)
 # class Expense(models.Model):
+class Expense(models.Model):
+  Expense_Plan_ID = models.ForeignKey(Expense_Plan, on_delete = models.SET_NULL, null = True, blank = True)
+  Cost = models.FloatField(default = 0.0)
+  Expense_Type = models.CharField(max_length = 256)
+  Description = models.CharField(max_length = 256)
+  Expense_Date_Issued = models.DateField(auto_now_add = True)
 
-# class Employee(models.Model):
+#class Employee(models.Model):
 
-# class Moderates
+class Moderates(models.Model):
+  Site_Admin_ID = models.ForeignKey(Site_Admin, on_delete = models.CASCADE)
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
 
-# class RecurringPlan
+class RecurringPlan(models.Model):
+  Expense_Plan_ID = models.ForeignKey(Expense_Plan, on_delete = models.CASCADE)
+  Plan_Frequency = models.CharField(max_length = 64)
+  Occurance_Number = models.PositiveIntegerField(default = 1)
 
 # class Contract
 
-# class InvoiceLineItem
+class InvoiceLineItem(models.Model):
+  Invoice_ID = models.ForeignKey(Inovice, on_delete = models.CASCADE)
+  Line_Number = models.PositiveIntegerField()
+  Cost = models.FloatField(default = 0.0)
+  Description = models.CharField(max_length = 256)
+  Quantity = models.PositiveIntegerField(default = 1)
+  Header = models.CharField(max_length = 256)
 
-# class Business_Client
+class Business_Client(models.Model):
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
 
 # class Individual_Client
