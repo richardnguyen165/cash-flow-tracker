d4f41cf32497f567480610d336f3e0e0e21feb84
richard.nguyen1 <richard.nguyen2005@outlook.com>
2026-04-06 02:10:53 -0600

    Finished the tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
   # Note: id is automatically added
@@ -17,16 +18,26 @@ class Business(models.Model):
   Business_Name = models.CharField(max_length = 256)
   Business_Balance = models.FloatField(default = 0.0)
   Business_Profile = models.CharField(max_length = 256)
+
+# https://www.geeksforgeeks.org/python/autofield-django-models/
+# https://dev.to/zachtylr21/model-inheritance-in-django-m0j
+class Client(models.Model):
+  Client_ID = models.AutoField(primary_key=True)
+  
+class Business_Client(Client):
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  # User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Individual_Client(Client):
+  User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
   
 class Site_Admin(models.Model):
   User_ID = models.OneToOneField(User, on_delete = models.CASCADE)
-
 
 class Business_Admin(models.Model):
   User_ID = models.OneToOneField(User, on_delete = models.CASCADE)
   Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
 
-  
 class Transaction(models.Model):
   # Note: Foreign Key is beest used for one to many relationships
   Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
@@ -41,26 +52,42 @@ class Inovice(models.Model):
   Has_Paid = models.BooleanField(default = False)
   Policy_Description = models.CharField(max_length = 256)
 
-
 class Expense_Plan(models.Model):
   Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
   Plan_Title = models.CharField(max_length = 256)
   Expense_Plan_Due = models.DateField()
-# class Expense_Pay_Off(models.Model):
+  
 class Expense_Pay_Off(models.Model):
   Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
   Transaction_ID = models.OneToOneField(Transaction, on_delete = models.CASCADE)
   Total_Pay = models.FloatField(default = 0.0)
-# class Expense(models.Model):
+
 class Expense(models.Model):
   Expense_Plan_ID = models.ForeignKey(Expense_Plan, on_delete = models.SET_NULL, null = True, blank = True)
   Cost = models.FloatField(default = 0.0)
   Expense_Type = models.CharField(max_length = 256)
   Description = models.CharField(max_length = 256)
   Expense_Date_Issued = models.DateField(auto_now_add = True)
+  Expense_Due_By = models.DateField(null = False, blank = False)
 
-#class Employee(models.Model):
+class Employee(models.Model):
+  User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
+  Business_ID = models.ForeignKey(Business, on_delete = models.SET_NULL)
+  Expense_Plan_ID = models.ForeignKey(Expense_Plan, on_delete = models.SET_NULL, null = True, blank = True)
+  Expense_ID = models.ForeignKey(Expense, on_delete = models.SET_NULL, null = True, blank = True)
+  Pay = models.FloatField(default = 0.0)
+  Role = models.CharField(max_length = 64)
 
+class Contract(models.Model):
+  # User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
+  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
+  Client_ID = models.ForeignKey(Client, on_delete=models.CASCADE)
+  Expense_Plan_ID = models.ForeignKey(Expense_Plan, on_delete = models.SET_NULL)
+  Expense_ID = models.ForeignKey(Expense, on_delete = models.SET_NULL)
+  Duration = models.PositiveIntegerField(default = 1)
+  Name = models.CharField(max_length=64)
+  Terms = models.CharField(max_length=5000)
+  
 class Moderates(models.Model):
   Site_Admin_ID = models.ForeignKey(Site_Admin, on_delete = models.CASCADE)
   Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
@@ -70,8 +97,6 @@ class RecurringPlan(models.Model):
   Plan_Frequency = models.CharField(max_length = 64)
   Occurance_Number = models.PositiveIntegerField(default = 1)
 
-# class Contract
-
 class InvoiceLineItem(models.Model):
   Invoice_ID = models.ForeignKey(Inovice, on_delete = models.CASCADE)
   Line_Number = models.PositiveIntegerField()
@@ -79,9 +104,3 @@ class InvoiceLineItem(models.Model):
   Description = models.CharField(max_length = 256)
   Quantity = models.PositiveIntegerField(default = 1)
   Header = models.CharField(max_length = 256)
-
-class Business_Client(models.Model):
-  Business_ID = models.ForeignKey(Business, on_delete = models.CASCADE)
-  User_ID = models.ForeignKey(User, on_delete = models.CASCADE)
-
-# class Individual_Client
